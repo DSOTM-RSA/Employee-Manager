6c2d4ebf89ee14f23e8a0db941424d3d1959c2f5
Dan Gray <eoe.ddg.deu@gmail.com>
2020-12-04 18:21:50 +0100

    Add project files.

---
+++ EmployeeManager/Controllers/EmployeeManagerController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeManager.Data;
+using EmployeeManager.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace EmployeeManager.Controllers
+{
+    public class EmployeeManagerController : Controller
+    {
+
+        private AppDbContext _db;
+        public EmployeeManagerController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+
+        public IActionResult List()
+        {
+            List<Employee> model = (from e in _db.Employees
+                                    orderby e.EmployeeID
+                                    select e).ToList();
+
+            return View(model);
+        }
+
+
+        [HttpGet]
+        public IActionResult Insert()
+        {
+            FillCountries();
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Insert(Employee model)
+        {
+            FillCountries();
+
+            if (ModelState.IsValid)
+            {
+                _db.Employees.Add(model);
+                _db.SaveChanges();
+                ViewBag.Message = "Employee added successfully";
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            FillCountries();
+            var model = _db.Employees.Find(id);
+            return View(model);
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(Employee model)
+        {
+            FillCountries();
+
+            if (ModelState.IsValid)
+            {
+                _db.Employees.Update(model);
+                _db.SaveChanges();
+                ViewBag.Message = "Employee updated successfully";
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        [ActionName("Delete")]
+        public IActionResult ConfirmDelete(int id)
+        {
+            var model = _db.Employees.Find(id);
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            var model = _db.Employees.Find(id);
+            _db.Employees.Remove(model);
+            _db.SaveChanges();
+
+            TempData["Message"] = "Employee Deleted Successfully";
+
+            return RedirectToAction("List");
+        }
+
+
+        private void FillCountries()
+        {
+            List<SelectListItem> listOfCountries = (from c in _db.Employees
+                                                    select new SelectListItem()
+                                                    {
+                                                        Text = c.Country,
+                                                        Value = c.Country
+                                                    }).Distinct().ToList();
+
+            ViewBag.Countries = listOfCountries;
+        }
+
+
+    }
+}
---
+++ EmployeeManager/Models/Employee.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManager.Models
+{
+    [Table("Employees")]
+    public class Employee
+    {
+        [Column("EmployeeID")]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Required(ErrorMessage ="Employee ID is required")]
+        [Display(Name ="Employee ID")]
+        public int EmployeeID { get; set; }
+
+        [Column("FirstName")]
+        [Display(Name = "First Name")]
+        [StringLength(20,ErrorMessage ="First Name must be less than 20 characters")]
+        [Required(ErrorMessage = "Employee First Name is required")]
+        public string FirstName { get; set; }
+
+        [Column("LastName")]
+        [Display(Name = "Last Name")]
+        [StringLength(30, ErrorMessage = "Last Name must be less than 30 characters")]
+        [Required(ErrorMessage = "Employee Last Name is required")]
+        public string LastName { get; set; }
+
+        [Column("Title")]
+        public string Title { get; set; }
+
+        [Column("BirthDate")]
+        [Display(Name = "Birth Dat")]
+        [Required(ErrorMessage = "Birth Date is required")]
+        public DateTime BirthDate { get; set; }
+
+        [Column("HireDate")]
+        [Display(Name = "Hire Date")]
+        [Required(ErrorMessage = "Hire Date is required")]
+        public DateTime HireDate { get; set; }
+
+        [Column("Country")]
+        [Required(ErrorMessage = "Country is required")]
+        public string Country { get; set; }
+
+        [Column("Notes")]
+        public string Notes { get; set; }
+
+    }
+}
